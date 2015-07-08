9aeb53a1bf79cf295ae07c29a0d3a5e97001debf
qkdtp12 <qkdtp12@gmail.com>
2015-07-09 02:09:44 +0900

    2
    2

--- RealServer/Connect/routes/contents.js
+++ RealServer/Connect/routes/contents.js
@@ -6,9 +6,23 @@ var connection = mysql.createConnection({
     'user' : 'LINKBOX', 
     'password' : 'dlrpqkfhdnflek', 
     'database' : 'LINKBOX', });
-router.get('/', function(req, res, next) {
-connection.query('select id, title, timestamp from board ' +
-'order by timestamp desc;', function (error, cursor) {
-res.json(cursor); });
+
+router.get('/:content_id', function(req, res, next) {
+connection.query('select * from board where id=?;', [req.params.content_id], function (error, cursor) {
+if (cursor.length > 0) res.json(cursor[0]);
+else
+res.status(503).json({ result : false, reason : "Cannot find selected article" });
+}); });
+router.post('/', function(req, res, next) {
+connection.query('insert into board(title, content) values (?, ?);', [req.body.title, req.body.content], function (error, info) {
+if (error == null) {
+connection.query('select * from board where id=?;', [info.insertId], function (error, cursor) {
+if (cursor.length > 0) { res.json({
+result : true, id : cursor[0].id, title : cursor[0].title,
+timestamp : cursor[0].timestamp, });
+} else
+res.status(503).json({ result : false, reason : "Cannot post article" }); });
+} else
+res.status(503).json(error); });
 });
 module.exports = router;
