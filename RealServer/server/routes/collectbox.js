9e962078fde82c36889364a220373992b06e1ea1
qkdtp12 <qkdtp12@gmail.com>
2015-07-08 22:14:24 +0900

    a
    a

--- RealServer/server/routes/collectbox.js
+++ RealServer/server/routes/collectbox.js
@@ -27,37 +27,5 @@ router.get('/{usrid}/cbid', function(req, res, next) {
         });
 });
 
-router.post('/{usrid}/addbox', function(req, res, next) {
-    
-    connection.query('insert into collectbox(cbname) values (?)', [req.body.cbname], function (error, info) {
-        if (error == null) {
-            connection.query('select * from board where id=?;', [info.insertId], function (error, cursor) {
-                               
-                               if (cursor.length > 0) {
-
-                                        var result = cursor[0];
-
-                                        res.json({
-												result : true,
-                                                cbname : result.cbname
-                                        });
-                                }
-                                else {
-
-                                        res.status(503).json({
-						
-											result : false,
-											reason : "Cannot post article"
-										});
-                                }
-                        });
-                }
-                else {
-
-                        res.status(503).json(error);
-                }
-    }
-});
-
 
 module.exports = router;
