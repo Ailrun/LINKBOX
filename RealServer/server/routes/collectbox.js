9012f3d9bd720e54baec53789d50ead12082e16f
wlgus1644 <dlwlgus1644@naver.com>
2015-07-10 07:57:26 +0900

    d
    d

--- RealServer/server/routes/collectbox.js
+++ RealServer/server/routes/collectbox.js
@@ -16,7 +16,7 @@ router.post('/:usrid/addbox', function(request, response, next){
 
     connection.query('SELECT MAX(cbid) as max from collectbox;', function(error, cursor){
     connection.query('INSERT INTO collectbox (cbid, cbname) values(?,?);', [cursor[0].max+1, request.body.cbname], function(error, info) {
-        log.console(error);
+        console.log(error);
             if(error !== undefined)
                 response.sendStatus(503);
         
