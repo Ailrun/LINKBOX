3758b5bf2d9ed61d5f19211eae1f0493ed257f0a
qkdtp12 <qkdtp12@gmail.com>
2015-07-10 20:44:13 +0900

    둥

--- RealServer/server/routes/share.js
+++ RealServer/server/routes/share.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 //collectbox에 공유하고자 하는 usr추가
 router.post('/:cbid/addusr/:usremail', function(req, res, next){
     connection.query('SELECT usrid FROM usr WHERE usremail = ?;', [req.params.usremail], function(erroe, cursor){
-        connection.query('insert into share (usrid, cbid) values (?, ?);', [cursor.usrid, req.params.cbid], function(error, info) {
+        connection.query('insert into share (usrid, cbid) values (?, ?);', [cursor[0].usrid, req.params.cbid], function(error, info) {
         if(error != undefined){
             res.sendStatus(503);
         }
