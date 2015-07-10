15162b3c8cc1f456dc903910e92fffd1d1bef2e9
qkdtp12 <qkdtp12@gmail.com>
2015-07-10 22:20:00 +0900

    s
    s

--- RealServer/server/routes/share.js
+++ RealServer/server/routes/share.js
@@ -28,7 +28,7 @@ router.post('/:cbid/addusr', function(req, res, next){
 });
 
 //
-router.get('/:cbid/boxlist', function(req, res, next) {
+router.get('/:cbid/urllist', function(req, res, next) {
   
    connection.query('SELECT usrid, usrname, usremail, usrprofile FROM usr WHERE cbid=?;', [req.params.cbid], function (error, cursor) {
       
