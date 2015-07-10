b62ea1843b76e8d4152171797f81e10154634f7c
qkdtp12 <qkdtp12@gmail.com>
2015-07-10 23:03:20 +0900

    ee
    ee

--- RealServer/server/routes/share.js
+++ RealServer/server/routes/share.js
@@ -32,7 +32,7 @@ router.get('/:cbid/urllist', function(req, res, next) {
     
     connection.query('SELECT usrid FROM share WHERE cbid = ?;', [req.params.cbid], function (error, cursor){
         console.log(cursor)
-        connection.query('SELECT usrid, usrname, usremail, usrprofile FROM usr where usrid = ?;', [cursor], function (error, info){
+        connection.query('SELECT usrid, usrname, usremail, usrprofile FROM usr where usrid = ?;', [cursor.usrid], function (error, info){
         
             if(error != undefined){
                 res.sendStatus(503);
