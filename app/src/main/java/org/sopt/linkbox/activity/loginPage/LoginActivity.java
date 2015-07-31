e04c50d8b098109c9b9dee1b35fe03be23b81459
Ailrun <jjc9310@gmail.com>
2015-08-01 03:13:52 +0900

    Change layout of Accounts

--- app/src/main/java/org/sopt/linkbox/activity/loginPage/LoginActivity.java
+++ app/src/main/java/org/sopt/linkbox/activity/loginPage/LoginActivity.java
@@ -1,55 +1,34 @@
 package org.sopt.linkbox.activity.loginPage;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 import org.sopt.linkbox.R;
 import org.sopt.linkbox.activity.loadingPage.LoginLoadingActivity;
 import org.sopt.linkbox.constant.LoginStrings;
-import org.sopt.linkbox.constant.SettingStrings;
-import org.sopt.linkbox.debugging.FacebookDebug;
-
-import java.util.Collections;
 
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "TEST/" + LoginActivity.class.getName() + " : ";
 
+    private Toolbar tToolbar = null;
+    private EditText etEmail = null;
     private EditText etPass = null;
-    private AutoCompleteTextView actUsremail = null;
-    private Button bLogin = null;
-    private LoginButton lbFacebookLogin = null;
-
-    private SharedPreferences spProfile = null; // 02 Save preference or use preference that is saved. Bunched up data
-    private SharedPreferences.Editor speProfile = null;
-    private CallbackManager callbackManager = null;
+    private ImageButton ibLogin = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-
-        initData();
-
-        initAutoLogin();
 
         initView();
         initListener();
@@ -57,45 +36,23 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        if(AccessToken.getCurrentAccessToken() !=null){
-            startActivity(new Intent(getApplicationContext(), FacebookDataActivity.class));
-            finish();
-        }
-        else{
-            lbFacebookLogin.setVisibility(View.VISIBLE);
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 
-    private void initData() {
-        spProfile = getSharedPreferences(SettingStrings.shared_user_profiles, 0);
-        speProfile = spProfile.edit();  // 03 Shared preference cannot edit data.
-    }
-    private void initAutoLogin() {
-        String usremail = spProfile.getString(LoginStrings.usrID, "");
-        String pass = spProfile.getString(LoginStrings.usrPassword, "");
-        if (usremail != null && !usremail.equals("") && pass != null && !pass.equals("")) {
-            loginLoading(usremail, pass);
+    private void initView()  {
+        tToolbar = (Toolbar) findViewById(R.id.T_toolbar_login);
+        setSupportActionBar(tToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-    }
-    private void initView() {
-        actUsremail = (AutoCompleteTextView) findViewById(R.id.ACT_usremail_main);
-        etPass = (EditText) findViewById(R.id.ET_password_main);
-        bLogin = (Button) findViewById(R.id.B_login_main);
-        lbFacebookLogin = (LoginButton) findViewById(R.id.LB_login_main);
+        etEmail = (EditText) findViewById(R.id.ET_email_login);
+        etPass = (EditText) findViewById(R.id.ET_password_login);
+        ibLogin = (ImageButton) findViewById(R.id.IB_login_login);
     }
     private void initListener() {
-        callbackManager = CallbackManager.Factory.create();
-        lbFacebookLogin.setReadPermissions(Collections.singletonList("email"));
-        lbFacebookLogin.registerCallback(callbackManager, new FacebookLoginCallback()); // Facebook button
-        bLogin.setOnClickListener(new View.OnClickListener() {
+        ibLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usremail = actUsremail.getText().toString();
+                String usremail = etEmail.getText().toString();
                 String pass = etPass.getText().toString();
                 if (usremail.equals("") || pass.equals("")) {
                     Toast.makeText(getApplicationContext(), "All Field must be filled!", Toast.LENGTH_LONG).show();
@@ -112,23 +69,5 @@ public class LoginActivity extends AppCompatActivity {
         intent.putExtra(LoginStrings.usrPassword, pass);
         startActivity(intent);
         finish();
-    }
-
-    //Inner Class
-    private class FacebookLoginCallback implements FacebookCallback<LoginResult> {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            Intent intent = new Intent(getApplicationContext(), FacebookDataActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        @Override
-        public void onCancel() {
-            Log.d(TAG, "I cancel U!");
-        }
-        @Override
-        public void onError(FacebookException e) {  // TODO: FACEBOOK ERROR NEEDS AN UPDATE
-            FacebookDebug.debug(e);
-        }
     }
 }
--- app/src/main/java/org/sopt/linkbox/activity/loginPage/SignupActivity.java
+++ app/src/main/java/org/sopt/linkbox/activity/loginPage/SignupActivity.java
@@ -2,33 +2,32 @@ package org.sopt.linkbox.activity.loginPage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.sopt.linkbox.R;
 
 
-public class SignupActivity extends Activity {
-    private static final String SERVER_ADDRESS = "http://52.68.70.129:8080/";
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "TEST/" + SignupActivity.class.getName() + " : ";
+
+    private Toolbar tToolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_signup, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
+        initView();
+    }
+
+    private void initView() {
+        tToolbar = (Toolbar) findViewById(R.id.T_toolbar_signup);
+        setSupportActionBar(tToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
