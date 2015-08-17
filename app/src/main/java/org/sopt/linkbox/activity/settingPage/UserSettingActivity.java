4dfc84da65c019bd0dae891e327bfd44585b9fff
MinGu <projgotham@naver.com>
2015-08-18 02:53:36 +0900

    Linkbox Setting Page

--- app/src/main/java/org/sopt/linkbox/activity/settingPage/UserSettingActivity.java
+++ app/src/main/java/org/sopt/linkbox/activity/settingPage/UserSettingActivity.java
@@ -60,7 +60,6 @@ public class UserSettingActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_user_setting);
 
-
         initInterface();
         initData();
         initView();
