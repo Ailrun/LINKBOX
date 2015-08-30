60c6a574d45c8219e9926934a478b3c6a0091363
CanKitty <popc94@naver.com>
2015-08-30 13:16:45 +0900

    WebView #5

--- app/src/main/java/org/sopt/linkbox/custom/adapters/listViewAdapter/WebviewReplyListAdapter.java
+++ app/src/main/java/org/sopt/linkbox/custom/adapters/listViewAdapter/WebviewReplyListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -15,7 +16,9 @@ import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
 import org.sopt.linkbox.R;
 import org.sopt.linkbox.custom.data.mainData.UsrListData;
 import org.sopt.linkbox.custom.data.mainData.url.CommentListData;
+import org.sopt.linkbox.custom.data.mainData.url.UrlListData;
 import org.sopt.linkbox.custom.helper.ViewHolder;
+import org.sopt.linkbox.custom.network.main.url.UrlListWrapper;
 import org.sopt.linkbox.custom.widget.RoundedImageView;
 
 import java.io.File;
@@ -28,6 +31,8 @@ public class WebviewReplyListAdapter extends BaseAdapter {
     private ArrayList<CommentListData> source = null;
     private LayoutInflater layoutInflater = null;
     private Context context = null;
+    private CommentListData commentListData = null;
+    private UrlListWrapper urlListWrapper = null;
 
     public WebviewReplyListAdapter(Context context, ArrayList<CommentListData> source) {
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -69,13 +74,24 @@ public class WebviewReplyListAdapter extends BaseAdapter {
             view = layoutInflater.inflate(R.layout.layout_reply_list_webview, viewGroup, false);
         }
 
-        CommentListData commentListData = (CommentListData) getItem(i);
+        commentListData = (CommentListData) getItem(i);
         RoundedImageView ivProfile = ViewHolder.get(view, R.id.IV_profile_reply_webview);
         TextView tvName = ViewHolder.get(view, R.id.TV_name_reply_webview);
         TextView tvContents = ViewHolder.get(view, R.id.TV_contents_reply_webview);
+        final ImageButton ibDelete = ViewHolder.get(view, R.id.IB_reply_delete_webview);
+
         Glide.with(context).load(commentListData.usrThumbnail).into(ivProfile);
         tvName.setText(commentListData.usrName);
         tvContents.setText(commentListData.comment);
+
+        ibDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 여기는 어떻게하나
+                //urlListWrapper.commentRemove((UrlListData)getItem(i), (CommentListData)commentListData, new ReplyDelete());
+                ibDelete.setEnabled(false);
+            }
+        });
         return view;
     }
 
